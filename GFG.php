5226fb441863f7c0234b9daf5bd4a7593252fdf5
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2020-01-23 13:39:40 -0500

    Finished example, now to dissect

--- GFG.php
+++ GFG.php
@@ -6,10 +6,25 @@ class GFG {
     private $pwd;
 
     // update GFG password
-    public function updatePWd($userId, $pwd) {
+    public function updatePwd($userId, $pwd) {
         //Write function body
-        
+        echo("Function to update password '" . $pwd . "' for user " . $userId);
+
+        echo "<br>";
 
     }
 
+    //Check account balance
+    public function courseName ($userId) {
+        //write function body
+        echo ("Function to check course name of user "
+        .$userId);
+        
+        echo "<br>";
+    }
+
 }
+
+$object = new GFG();
+$object -> updatePwd('GFG12', 'geeks54321');
+$object -> courseName('GFG06');
