91917520a5cc8a4baf39d055c6ad3ca9cd0ce500
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2020-01-23 13:17:36 -0500

    Learning encapsulation

---
+++ GFG.php
@@ -0,0 +1,15 @@
+<?php
+// A php program to implement encapsulation
+
+class GFG {
+    private $userId;
+    private $pwd;
+
+    // update GFG password
+    public function updatePWd($userId, $pwd) {
+        //Write function body
+        
+
+    }
+
+}
--- fruit.php
+++ fruit.php
@@ -10,9 +10,15 @@ class Fruit {
     public function intro() {
         echo "The fruit is {$this->name} and the color is {$this->color}.";
     }
+}
 
     //Strawberry is inherited from Fruit
     class Strawberry extends Fruit {
-        
+        public function message() {
+            echo "Am I a fruit or a berry?";
+        }
     }
-}
+
+    $strawberry = new Strawberry("Strawberry", "red");
+    $strawberry->message();
+    $strawberry->intro();
