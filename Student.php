69579b7af3f1b87cd32652fa60f5cd847add0446
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2020-01-23 13:54:57 -0500

    Finished following example

--- Student.php
+++ Student.php
@@ -14,6 +14,20 @@ class Student {
     }
 
     public function getGender(){
-        return $this->gender
+        return $this->gender;
+    }
+
+    public function setGender($gender) {
+        if('Male' !== $gender and 'Female' !== $gender) {
+            echo ('Set gender as Male or Female for gender');
+        }
+
+        $this->gender = $gender;
+        echo("Gender is set to " . $gender);
+        echo ("<br>");
     }
 }
+
+$student = new Student();
+$student->setFirstName('Meena');
+$student->setGender('Female');
